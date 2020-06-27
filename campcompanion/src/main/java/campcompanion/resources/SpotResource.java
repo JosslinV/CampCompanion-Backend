f9937f706c30c8a3a3382ce307d7ddd0cf571fec
Josslin Varon <josslinvaron@gmail.com>
2020-06-27 17:15:55 +0200

    Add CRUD for spot

--- campcompanion/src/main/java/campcompanion/resources/SpotResource.java
+++ campcompanion/src/main/java/campcompanion/resources/SpotResource.java
@@ -1,8 +1,205 @@
 package campcompanion.resources;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import campcompanion.hibernate.HibernateUtils;
+import campcompanion.model.Spot;
 
 @Path("spot")
 public class SpotResource {
+	
+	/********************************
+	 * 
+	 * 	ALL C R U D OPERATIONS
+	 * 
+	 ********************************/
+	
+	// C stands for C R E A T E !
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response create(String spot) {
+		ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			Spot spotAdd = objectMapper.readValue(spot , Spot.class);
+			session.save(spotAdd);
+			tx.commit();
+
+			return Response.status(200).entity(spot).build();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+			return Response.serverError().build();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		} finally {
+			session.close();
+		}
+	}
+	
+	// R stands for R E A D !
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response read() {
+		ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			@SuppressWarnings("rawtypes")
+			List spots = session.createQuery("FROM Spot").list();
+			tx.commit();
+			
+			String arrayToJson = objectMapper.writeValueAsString(spots);
+			return Response.status(200).entity(arrayToJson).build();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+			return Response.serverError().build();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		} finally {
+			session.close();
+		}
+	}
+	
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response readOne(@PathParam("id") String id) {
+		ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+				tx = session.beginTransaction();
+				Spot spot = (Spot) session.get(Spot.class, Integer.parseInt(id));
+				tx.commit();
+				
+			if(spot != null) {
+				String objectToJson = objectMapper.writeValueAsString(spot);
+				return Response.status(200).entity(objectToJson).build();
+			} else {
+				return Response.status(500).entity("Spot with id " + id + " doesn't exist.").build();
+			}
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+			return Response.serverError().build();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		} finally {
+			session.close();
+		}
+    }
+	
+	// U stands for U P D A T E !
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response update(@PathParam("id") String id, String userInfo) {
+		ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			Spot spot = (Spot) session.get(Spot.class, Integer.parseInt(id));
+			if(spot != null) {
+				Spot spotMod = objectMapper.readValue(userInfo , Spot.class);
+				
+				spot.setName(spotMod.getName());
+				spot.setLatitude(spotMod.getLatitude());
+				spot.setLongitude(spotMod.getLongitude());
+				spot.setAccessibilityNote(spotMod.getAccessibilityNote());
+				spot.setUtilitiesNote(spotMod.getUtilitiesNote());
+				spot.setLocationNote(spotMod.getLocationNote());
+				spot.setPrivacyNote(spotMod.getPrivacyNote());
+				
+				session.update(spot);
+				tx.commit();
+				
+				String objectToJson = objectMapper.writeValueAsString(spot);
+				return Response.status(200).entity(objectToJson).build();
+			} else {
+				return Response.status(500).entity("Spot with id " + id + " doesn't exist.").build();
+			}
+
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+			return Response.serverError().build();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		} finally {
+			session.close();
+		}
+    }
+	
+	// D stands for D E L E T E !
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") String id) {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			Spot spot = (Spot) session.get(Spot.class, Integer.parseInt(id));
+			session.delete(spot);
+			tx.commit();
+			
+			return Response.ok().entity(id).build();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+			return Response.serverError().build();
+		} finally {
+			session.close();
+		}
+    }
 
 }
